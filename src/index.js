db022e3c5e963758684a5f12a65dd3b28a3954c7
Pedro Aim <pedroi@criptext.com>
2017-12-20 11:25:16 -0500

    working react app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
+
 registerServiceWorker();
