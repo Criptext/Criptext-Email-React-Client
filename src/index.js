88326b9c45ba9867d84604acce047af9a20977fb
Pedro Aim <pedroi@criptext.com>
2017-12-20 11:49:12 -0500

    added react-router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './reducers/index'
+import rootReducer from './reducers/index';
+import {  BrowserRouter as Router, Route } from 'react-router-dom' 
 
 const store = createStore(rootReducer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>, 
 document.getElementById('root'));
 
