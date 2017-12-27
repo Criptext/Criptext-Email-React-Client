c02e55b8ac4fcf5145525b97e34085a3cf893393
Pedro Aim <pedroi@criptext.com>
2017-12-27 10:40:36 -0500

    icons function fix

--- src/components/ThreadView.js
+++ src/components/ThreadView.js
@@ -20,22 +20,44 @@ const ThreadView = props => {
       </div>
       <div>
         <div>{thread.preview}</div>
-        <div>
-          {thread.timesOpened > 0 ? (
-            <i className="material-icons">drafts</i>
-          ) : null}
-          {thread.timesOpened > 0 ? thread.timesOpened : null}
-          {thread.totalAttachments > 0 ? (
-            <i className="material-icons">attach_file</i>
-          ) : null}
-          {thread.totalAttachments > 0 ? thread.totalAttachments : null}
-          {thread.hasOpenAttachments ? (
-            <i className="material-icons">file_download</i>
-          ) : null}
+        <div className='thread-icons'>
+          {willDisplayOpenIcon(thread)}
+          {willDisplayAttachIcon(thread)}
+          {willDisplayOpenAttachIcon(thread)}
         </div>
       </div>
     </div>
   );
 };
+
+const willDisplayOpenIcon = thread => {
+  if(thread.timesOpened > 0){
+    return (<div>
+      <i className="material-icons">drafts</i>
+      {thread.timesOpened}
+    </div>)
+  }
+
+  return null
+}
+
+const willDisplayAttachIcon = thread => {
+  if(thread.totalAttachments > 0){
+    return (<div>
+      <i className="material-icons">attach_file</i>
+      {thread.totalAttachments}
+    </div>)
+  }
+
+  return null
+}
+
+const willDisplayOpenAttachIcon = thread => {
+  if(thread.timesOpened > 0){
+    return <i className="material-icons">file_download</i>;
+  }
+
+  return null
+}
 
 export default ThreadView;
