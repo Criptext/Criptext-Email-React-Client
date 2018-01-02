8c652649c61c707a484a422b5c6057f2dc7a30cd
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-02 17:29:23 -0500

    Add ActivityPanel component

---
+++ src/components/ActivityPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './activitypanel.css';
+
+const ActivityPanel = props => (
+  <aside className="navigation-feed">
+    <header>
+    </header>
+  </aside>
+);
+
+export default ActivityPanel;
