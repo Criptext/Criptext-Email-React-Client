510b0847da6a1d034fd5d8001b302f2e5cb71889
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-03 12:07:02 -0500

    Add icons as font

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,47 +9,80 @@ const SideBar = props => (
     </header>
     <div className="navigation-partial-mail">
       <div className="nav-item-free">
-        <button className="button button-compose">Compose</button>
+        <button className="button button-compose">
+          <i className="icon-edit" />
+          <span>Compose</span>
+        </button>
       </div>
       <nav>
         <ul>
           <li className="nav-item nav-item-selected">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-mailbox" />
+            </div>
             <Link to="/inbox">Inbox</Link>
           </li>
           <li className="nav-item">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-sent" />
+            </div>
             <Link to="/">Sent</Link>
             <div className="nav-notif">
               <span>+99</span>
             </div>
           </li>
           <li className="nav-item">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-doc" />
+            </div>
             <Link to="/">Draft</Link>
+            <div className="nav-notif">
+              <span>10</span>
+            </div>
           </li>
           <li className="nav-item">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-start" />
+            </div>
             <Link to="/">Starred</Link>
           </li>
           <li className="nav-item">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-not" />
+            </div>
             <Link to="/">Spam</Link>
           </li>
           <li className="nav-item">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-trash" />
+            </div>
             <Link to="/">Trash</Link>
           </li>
           <li className="nav-item">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-calendar" />
+            </div>
             <Link to="/">Send Later</Link>
           </li>
+          <li className="nav-item">
+            <div className="nav-item-icon">
+              <i className="icon-bell-inclined" />
+            </div>
+            <Link to="/">Reminders</Link>
+            <div className="nav-notif">
+              <span>3</span>
+            </div>
+          </li>
           <li className="nav-item nav-item-expand">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-file" />
+            </div>
             <Link to="/">Folders</Link>
           </li>
           <li className="nav-item nav-item-expand">
-            <div className="nav-item-icon" />
+            <div className="nav-item-icon">
+              <i className="icon-tag" />
+            </div>
             <Link to="/">Labels</Link>
           </li>
         </ul>
