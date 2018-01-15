c02aeb4568258154899a0c6d45e4019a68f96b4f
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-15 15:19:56 -0500

    Divide function to render email by collapse and expand view

--- src/components/Email.js
+++ src/components/Email.js
@@ -3,51 +3,57 @@ import PropTypes from 'prop-types';
 import './email.css';
 
 const Email = props =>
-  props.isOpen ? (
-    <div className="email-container email-container-expand">
-      <div className="email-info">
-        <div className="email-icon-letter">
-          <span>DM</span>
-        </div>
-        <div className="email-header-info">
-          <span>{props.email.get('from')}</span>
-          <span>Allison, Daniel, Gabriel, 2 others</span>
-        </div>
-        <div className="email-detail-info">
-          <span>{props.email.get('date')}</span>
-          <i className="icon-bell" />
-          <i className="icon-bell" />
-          <i className="icon-dots" />
-        </div>
+  props.isOpen
+    ? renderEmailExpand(props.email)
+    : renderEmailCollapse(props.email);
+
+const renderEmailCollapse = email => (
+  <div className="email-container email-container-collapse">
+    <span className="email-from">{email.get('from')}</span>
+    <span className="email-preview-content">
+      Lorem Ipsum is simply dummy text of the printing and
+    </span>
+    <div />
+    <span className="email-date">{email.get('date')}</span>
+  </div>
+);
+
+const renderEmailExpand = email => (
+  <div className="email-container email-container-expand">
+    <div className="email-info">
+      <div className="email-icon-letter">
+        <span>DM</span>
       </div>
-      <div className="email-body">
-        <div className="email-options">
-          <div>
-            <i className="icon-attach" />
-          </div>
-          <div>
-            <i className="icon-checked" />
-          </div>
-          <button className="button-a button-unsend">
-            <i className="icon-sent" />
-            <span>unsend</span>
-          </button>
-        </div>
-        <div className="email-text">
-          <p>Lorem Ipsum is simply dummy text of the printing and</p>
-        </div>
+      <div className="email-header-info">
+        <span>{email.get('from')}</span>
+        <span>Allison, Daniel, Gabriel, 2 others</span>
+      </div>
+      <div className="email-detail-info">
+        <span>{email.get('date')}</span>
+        <i className="icon-bell" />
+        <i className="icon-bell" />
+        <i className="icon-dots" />
       </div>
     </div>
-  ) : (
-    <div className="email-container email-container-collapse">
-      <span className="email-from">{props.email.get('from')}</span>
-      <span className="email-preview-content">
-        Lorem Ipsum is simply dummy text of the printing and
-      </span>
-      <div />
-      <span className="email-date">{props.email.get('date')}</span>
+    <div className="email-body">
+      <div className="email-options">
+        <div>
+          <i className="icon-attach" />
+        </div>
+        <div>
+          <i className="icon-checked" />
+        </div>
+        <button className="button-a button-unsend">
+          <i className="icon-sent" />
+          <span>unsend</span>
+        </button>
+      </div>
+      <div className="email-text">
+        <p>Lorem Ipsum is simply dummy text of the printing and</p>
+      </div>
     </div>
-  );
+  </div>
+);
 
 Email.propTypes = {
   email: PropTypes.object
