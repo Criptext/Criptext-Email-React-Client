0b78e1c4b207ffa25e1797a7b04fe8b9a97dc3df
Julian Adams <jadams@espol.edu.ec>
2018-01-15 17:55:58 -0500

    Ordering data in divs

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,9 +6,9 @@ const Feed = props => (
     <div className="feed-content">
       <div className="feed-icon">{props.renderIcon()}</div>
       <div className="feed-data"> 
-        <div className="feed-title">{props.feed.get('title')}</div>
-        <div className="feed-subject">{props.feed.get('subtitle')}</div>
-        <div className="feed-time">{props.feed.get('time')}</div>
+        <div className="feed-title"><span>{props.feed.get('title')}</span></div>
+        <div className="feed-subject"><span>{props.feed.get('subtitle')}</span></div>
+        <div className="feed-time"><span>{props.feed.get('time')}</span></div>
         <div className="feed-clear"></div>
       </div>
       <div className="feed-clear"></div>
