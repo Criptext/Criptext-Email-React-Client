f88021b12952ca0daa3c4b21f1b0da51ccae9965
Julian Adams <jadams@espol.edu.ec>
2018-01-17 15:28:00 -0500

    Feed type consistency

--- src/reducers/feeds.js
+++ src/reducers/feeds.js
@@ -1,15 +1,15 @@
-import * as Types from '../actions/types';
+import { Feed } from '../actions/types';
 import { List, Map } from 'immutable';
 
 export default (state = List([]), action) => {
   switch (action.type) {
-    case Types.Feed.ADD_BATCH: {
+    case Feed.ADD_BATCH: {
       const feeds = action.feeds.map(feed => {
         return Map(feed);
       });
       return state.concat(List(feeds));
     }
-    case Types.Feed.SELECT: {
+    case Feed.SELECT: {
       const item = state.find(feed => feed.get('id') === action.selectedFeed);
       if (item !== undefined) {
         const index = state.findIndex(
