34b6153ae20e5034d43f5455e0dbd060dff4f13e
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-19 18:22:26 -0500

    Add AttachItem component

---
+++ email_mailbox/src/components/AttachItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './attachitem.css';
+
+const AttachItem = props => (
+  <div className="attach-container">
+    <div
+      className="attach-preview"
+      style={{ backgroundImage: 'url(' + props.image + ')' }}
+    />
+    <div className="attach-data">
+      <i />
+      <span className="attach-data-name">Look at me.pdf</span>
+      <span className="attach-data-size">25MB</span>
+    </div>
+  </div>
+);
+
+AttachItem.propTypes = {
+  image: PropTypes.string
+};
+
+export default AttachItem;
