92fdc7beed2336b067a5b2831e83798d107abf91
Pedro Aim <pedroi@criptext.com>
2018-01-19 10:43:00 -0500

    new dir structure, working directories represent each module of the app + electron app

--- electron_app/electron-starter.js
+++ electron_app/electron-starter.js
@@ -10,7 +10,7 @@ function createWindow(){
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '/build/index.html'),
+    pathname: path.join(__dirname, '../email_mailbox/build/index.html'),
     protocol: 'file:',
     slashes: true
   });
