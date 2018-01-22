544dcbf13b0efa65c43ceaecdebb078da8aa4f6e
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-22 18:21:48 -0500

    Add replay, replay all and forward buttons

--- email_mailbox/src/components/Email.js
+++ email_mailbox/src/components/Email.js
@@ -26,49 +26,56 @@ const renderEmailCollapse = props => (
 );
 
 const renderEmailExpand = props => (
-  <div className="email-container email-container-expand">
-    <div className="email-info" onClick={props.onToggleEmail}>
-      <div className="email-icon-letter">
-        <span>DM</span>
+  <div>
+    <div className="email-container email-container-expand">
+      <div className="email-info" onClick={props.onToggleEmail}>
+        <div className="email-icon-letter">
+          <span>DM</span>
+        </div>
+        <div className="email-header-info">
+          <span className="email-header-info-from">
+            {props.email.get('from')}
+          </span>
+          <span className="email-header-info-to">
+            Allison, Daniel, Gabriel, 2 others
+          </span>
+        </div>
+        <div className="email-detail-info">
+          <span>{props.email.get('date')}</span>
+          <i className="icon-bell" />
+          <i className="icon-replay" />
+          <i className="icon-dots" />
+        </div>
       </div>
-      <div className="email-header-info">
-        <span className="email-header-info-from">
-          {props.email.get('from')}
-        </span>
-        <span className="email-header-info-to">
-          Allison, Daniel, Gabriel, 2 others
-        </span>
-      </div>
-      <div className="email-detail-info">
-        <span>{props.email.get('date')}</span>
-        <i className="icon-bell" />
-        <i className="icon-bell" />
-        <i className="icon-dots" />
+      <div className="email-body">
+        <div className="email-options">
+          <div>
+            <i className="icon-attach" />
+          </div>
+          <div>
+            <i className="icon-checked" />
+          </div>
+          <button className="button-a button-unsend">
+            <i className="icon-sent-left" />
+            <span>unsend</span>
+          </button>
+        </div>
+        <div className="email-text">
+          <p>Lorem Ipsum is simply dummy text of the printing and</p>
+        </div>
+        <div className="email-attachs">
+          <AttachItem
+            image={
+              'https://cdn-img-feed.streeteasy.com/nyc/image/50/300089950.jpg'
+            }
+          />
+        </div>
       </div>
     </div>
-    <div className="email-body">
-      <div className="email-options">
-        <div>
-          <i className="icon-attach" />
-        </div>
-        <div>
-          <i className="icon-checked" />
-        </div>
-        <button className="button-a button-unsend">
-          <i className="icon-sent" />
-          <span>unsend</span>
-        </button>
-      </div>
-      <div className="email-text">
-        <p>Lorem Ipsum is simply dummy text of the printing and</p>
-      </div>
-      <div className="email-attachs">
-        <AttachItem
-          image={
-            'https://cdn-img-feed.streeteasy.com/nyc/image/50/300089950.jpg'
-          }
-        />
-      </div>
+    <div className="email-segment-controls">
+      <div><i className="icon-replay"/><span>Replay</span></div>
+      <div><i className="icon-replay-all"/><span>Replay All</span></div>
+      <div><i className="icon-forward"/><span>Forward</span></div>
     </div>
   </div>
 );
