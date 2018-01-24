ab8b493f118215e8c1a68e90b36a9e46678819a6
Julian Adams <jadams@espol.edu.ec>
2018-01-24 18:19:36 -0500

    Adding flux components

--- email_login/src/App.js
+++ email_login/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+//import rootReducer from './reducers/index';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import './App.css';
+
+
+//const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+//      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </Router>
+//      </Provider>
     );
   }
 }
---
+++ email_login/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux-immutable';
+import login from './thread';
+
+export default combineReducers({
+  login
+});
