56491430ede589e7a3c026f9c6d617cb8f941c14
Julian Adams <jadams@espol.edu.ec>
2018-01-26 18:09:28 -0500

    Depuring

--- email_login/src/App.js
+++ email_login/src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-//import rootReducer from './reducers/index';
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
-import Login from './components/Login'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Login from './components/Login';
 import './app.css';
-
-//const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 class App extends Component {
   render() {
     return (
-//      <Provider store={store}>
-        <Router>
-          <Login />
-        </Router>
-//      </Provider>
+      <Router>
+        <Login />
+      </Router>
     );
   }
 }
--- email_login/src/components/Login.js
+++ email_login/src/components/Login.js
@@ -1,72 +1,67 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './login.css'
-
+import './login.css';
 
 const Login = () => renderLogin();
 
-
 const renderLogin = () => (
-	<div className="login">
-	  {renderHeader()}
-	  {renderForm()}
-	  {renderFooter()}
-	</div>
+  <div className="login">
+    {renderHeader()}
+    {renderForm()}
+    {renderFooter()}
+  </div>
 );
-
 
 const renderHeader = () => (
-	<div className="header">
-		<div className="logo">
-			<div className="icon"></div>
-		</div>
-		<div className="text">
-			<span>Welcome to Criptext!</span>
-		</div>
-	</div>
+  <div className="header">
+    <div className="logo">
+      <div className="icon" />
+    </div>
+    <div className="text">
+      <span>Welcome to Criptext!</span>
+    </div>
+  </div>
 );
-
 
 const renderForm = () => (
-	<div className="form">
-	  <form>
-	    <div className="label">
-		    <label>
-		      <input type="text" placeholder="Username" /> &nbsp;
-		      <span>@criptext.com</span>
-		      <div className="clear"></div>
-		    </label>
-	    </div>
-	    <div className="button">
-		    <button className="button-login">
-		      <span>Log In</span>
-		    </button>
-	    </div>
-	  </form>
-	</div>
+  <div className="form">
+    <form>
+      <div className="label">
+        <label>
+          <input type="text" placeholder="Username" /> &nbsp;
+          <span>@criptext.com</span>
+          <div className="clear" />
+        </label>
+      </div>
+      <div className="button">
+        <button className="button-login">
+          <span>Log In</span>
+        </button>
+      </div>
+    </form>
+  </div>
 );
-
 
 const renderFooter = () => (
-	<div className="footer">
-		<div className="signup-message">
-			<span>Not registered yet? 
-				&nbsp;
-				<Link to="/">
-					<strong>Sign up</strong>
-				</Link>
-			</span>
-		</div>
-		<div className="login-problems">
-			<span>
-				Can't 
-				<Link to="/">
-					<strong>Log In</strong>
-				</Link>
-				?</span>
-		</div>
-	</div>
+  <div className="footer">
+    <div className="signup-message">
+      <span>
+        Not registered yet? &nbsp;
+        <Link to="/">
+          <strong>Sign up</strong>
+        </Link>
+      </span>
+    </div>
+    <div className="login-problems">
+      <span>
+        Can&#39;t
+        <Link to="/">
+          <strong>Log In</strong>
+        </Link>
+        ?
+      </span>
+    </div>
+  </div>
 );
-
 
 export default Login;
