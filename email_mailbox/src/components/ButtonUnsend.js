0fbf6f8cd0d343c0fa2ca21cbcd11b9fbd760ca0
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-26 10:45:37 -0500

    Add ButtonUnsend component

---
+++ email_mailbox/src/components/ButtonUnsend.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './buttonunsend.css';
+
+const ButtonUnsend = props => (
+  <button
+    className={
+      'button-a button-unsend ' +
+      (props.displayLoading ? 'button-unsend-loading' : 'button-unsend-normal')
+    }
+    onClick={props.onClick}
+  >
+    {' '}
+    {props.displayLoading ? renderLoading() : renderButton(props.onClick)}
+  </button>
+);
+
+const renderButton = () => (
+  <div>
+    <i className="icon-unsend" />
+    <span>unsend</span>
+  </div>
+);
+
+const renderLoading = () => (
+  <div className="loading">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+ButtonUnsend.propTypes = {
+  displayLoading: PropTypes.bool,
+  onClick: PropTypes.func
+};
+
+export default ButtonUnsend;
