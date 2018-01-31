ea95335b413ad30be3c2b32829a6272cec201a46
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-31 09:44:25 -0500

    Add Subject component

---
+++ email_composer/src/components/Subject.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './subject.css';
+
+const Subject = () => (
+  <div className="subject-container">
+    <input type="text" placeholder="Subject" />
+  </div>
+);
+
+export default Subject;
