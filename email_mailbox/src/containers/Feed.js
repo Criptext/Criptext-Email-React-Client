dfddce74ab716c62253b6c8825434c001a0eb413
Julian Adams <jadams@espol.edu.ec>
2018-03-02 18:11:46 -0500

    Formatting

--- email_mailbox/src/containers/Feed.js
+++ email_mailbox/src/containers/Feed.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state, ownProps) => {
   return { isMuted, subtitle, title };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     onOpenThread: () => {
       dispatch();
