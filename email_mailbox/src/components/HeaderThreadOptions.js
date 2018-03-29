57cef774a68fd2674359be86b9e161ab1f9058f1
Erika Perugachi <erikaperugachi@gmail.com>
2018-03-29 13:26:55 -0500

    Header label options only shows custom labels

--- email_mailbox/src/components/HeaderThreadOptions.js
+++ email_mailbox/src/components/HeaderThreadOptions.js
@@ -139,17 +139,19 @@ class HeaderThreadOptions extends Component {
   };
 
   renderLabels = () => {
-    return this.props.labels.map((label, index) => (
-      <li key={index}>
-        <CustomCheckbox
-          onCheck={checked => {
-            this.onTriggerLabel(checked, label.id);
-          }}
-          label={label.text}
-          status={label.checked}
-        />
-      </li>
-    ));
+    return this.props.labels
+      .filter(label => label.id > 7)
+      .map((label, index) => (
+        <li key={index}>
+          <CustomCheckbox
+            onCheck={checked => {
+              this.onTriggerLabel(checked, label.id);
+            }}
+            label={label.text}
+            status={label.checked}
+          />
+        </li>
+      ));
   };
 }
 
