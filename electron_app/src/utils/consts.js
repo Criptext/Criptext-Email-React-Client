c9c379a6731c218e57947ced82c739720b1fa2a4
Erika Perugachi <erikaperugachi@gmail.com>
2018-05-30 12:17:34 -0500

    update installer

--- electron_app/src/utils/consts.js
+++ electron_app/src/utils/consts.js
@@ -1,9 +1,7 @@
 const PROD_SOCKET_URL = 'ws://stage.socket.criptext.com';
 const PROD_SERVER_URL = 'https://stage.mail.criptext.com';
-const RELEASE_VERSION = '0.2';
 
 module.exports = {
   PROD_SERVER_URL,
-  PROD_SOCKET_URL,
-  RELEASE_VERSION
+  PROD_SOCKET_URL
 };
