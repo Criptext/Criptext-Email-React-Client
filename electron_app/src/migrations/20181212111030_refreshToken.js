5aa9b0430c612139c99d8d3030ae75135bc9e750
Julian Adams <jadams@espol.edu.ec>
2018-12-12 18:58:32 -0500

    Database migration: Refresh token

---
+++ electron_app/src/migrations/20181212111030_refreshToken.js
@@ -0,0 +1,28 @@
+const { Table, fieldTypes } = require('./../models');
+const { XLARGE_STRING_SIZE } = fieldTypes;
+
+const createRefreshTokenColumn = knex => {
+  return knex.schema.table(Table.ACCOUNT, table => {
+    table.string('refreshToken', XLARGE_STRING_SIZE);
+  });
+};
+
+const deleteRefreshTokenColumn = knex => {
+  return knex.schema.table(Table.ACCOUNT, table => {
+    table.dropColumn('refreshToken');
+  });
+};
+
+exports.up = async (knex, Promise) => {
+  const checkAndCreateRefreshTokenColumn = knex.schema
+    .hasColumn(Table.ACCOUNT, 'refreshToken')
+    .then(columnExists => {
+      return !columnExists && createRefreshTokenColumn(knex);
+    });
+  return await Promise.all([checkAndCreateRefreshTokenColumn]);
+};
+
+// On rollback
+exports.down = async (knex, Promise) => {
+  return await Promise.all([deleteRefreshTokenColumn(knex)]);
+};
