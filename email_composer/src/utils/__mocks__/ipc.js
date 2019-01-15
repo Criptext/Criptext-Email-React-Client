86a0e77e2d49237b7b0f4ae528eaf58e25f84a07
erikaperugachi <erikaperugachi@gmail.com>
2019-01-15 18:21:07 -0500

    fix test

--- email_composer/src/utils/__mocks__/ipc.js
+++ email_composer/src/utils/__mocks__/ipc.js
@@ -1,4 +1,29 @@
+import { appDomain } from './const';
 import file from './../../../public/emails.json';
+
+const contacts = [
+  {
+    id: 1,
+    name: 'Usuario 1',
+    email: `toUser@${appDomain}`
+  },
+  {
+    id: 2,
+    name: 'Usuario 2',
+    email: `ccUser@${appDomain}`
+  },
+  {
+    id: 3,
+    name: 'Usuario 3',
+    email: `bccUser@${appDomain}`
+  },
+  {
+    id: 4,
+    name: 'Usuario 4',
+    email: `fromUser@${appDomain}`
+  }
+];
+
 const emails = file.emails;
 
 const files = [
@@ -26,6 +51,24 @@ const fileKeys = [
     emailId: emails[1].id
   }
 ];
+
+export const emailId = 1;
+
+export const getContactsByEmailId = id => {
+  const response = {
+    to: [{ email: contacts[0].email }],
+    cc: [{ email: contacts[1].email }],
+    bcc: [{ email: contacts[2].email }],
+    from: [{ email: contacts[3].email }]
+  };
+  const emptyResponse = {
+    to: [],
+    cc: [],
+    bcc: [],
+    from: [{ email: contacts[3].email }]
+  };
+  return id === emailId ? response : emptyResponse;
+};
 
 export const getEmailByKey = key => {
   return emails.filter(email => email.key === key);
