702c5f525ee1b7ee22bea88b31ca2b5a186fd872
Julián Adams <jadams@espol.edu.ec>
2019-02-28 17:21:58 -0500

    Fix empty mailboxes icons

--- email_mailbox/src/components/EmptyMailbox.js
+++ email_mailbox/src/components/EmptyMailbox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { LabelType } from '../utils/electronInterface';
 import string from './../lang';
 import './emptymailbox.scss';
 
 const EmptyMailbox = props => {
   const { header, subheader, iconClass } = defineEmptyParamsByMailbox(
-    props.mailbox.text
+    props.mailbox
   );
   return (
     <div className="empty-container empty-mailbox-container">
@@ -18,62 +19,68 @@ const EmptyMailbox = props => {
   );
 };
 
-const defineEmptyParamsByMailbox = mailbox => {
-  switch (mailbox) {
-    case 'Inbox':
+const defineEmptyParamsByMailbox = ({ id, text }) => {
+  switch (id) {
+    case LabelType.inbox.id:
       return {
         header: string.mailbox.empty.inbox.header,
         subheader: string.mailbox.empty.inbox.subheader,
         iconClass: 'empty-inbox'
       };
-    case 'Starred':
+    case LabelType.starred.id:
       return {
         header: string.mailbox.empty.starred.header,
         subheader: string.mailbox.empty.starred.subheader,
         iconClass: 'empty-starred'
       };
-    case 'Trash':
+    case LabelType.trash.id:
       return {
         header: string.mailbox.empty.trash.header,
         subheader: string.mailbox.empty.trash.subheader,
         iconClass: 'empty-trash'
       };
-    case 'Draft':
+    case LabelType.draft.id:
       return {
         header: string.mailbox.empty.draft.header,
         subheader: string.mailbox.empty.draft.subheader,
         iconClass: 'empty-draft'
       };
-    case 'Sent':
+    case LabelType.sent.id:
       return {
         header: string.mailbox.empty.sent.header,
         subheader: string.mailbox.empty.sent.subheader,
         iconClass: 'empty-sent'
       };
-    case 'Spam':
+    case LabelType.spam.id:
       return {
         header: string.mailbox.empty.spam.header,
         subheader: string.mailbox.empty.spam.subheader,
         iconClass: 'empty-spam'
       };
-    case 'All mail':
-      return {
-        header: string.mailbox.empty.allmail.header,
-        subheader: string.mailbox.empty.allmail.subheader,
-        iconClass: 'empty-allmail'
-      };
-    case 'Search':
-      return {
-        header: string.mailbox.empty.search.header,
-        subheader: string.mailbox.empty.search.subheader,
-        iconClass: 'empty-search'
-      };
-    default:
-      return {
-        header: string.mailbox.empty.default.header,
-        subheader: string.mailbox.empty.default.subheader,
-        iconClass: 'default-empty-mailbox'
-      };
+    default: {
+      switch (text) {
+        case 'Buscar':
+        case 'Search':
+          return {
+            header: string.mailbox.empty.search.header,
+            subheader: string.mailbox.empty.search.subheader,
+            iconClass: 'empty-search'
+          };
+        case 'All Mail':
+        case 'Todos':
+          return {
+            header: string.mailbox.empty.allmail.header,
+            subheader: string.mailbox.empty.allmail.subheader,
+            iconClass: 'empty-allmail'
+          };
+        default:
+          return {
+            header: string.mailbox.empty.default.header,
+            subheader: string.mailbox.empty.default.subheader,
+            iconClass: 'default-empty-mailbox'
+          };
+      }
+    }
   }
 };
 
