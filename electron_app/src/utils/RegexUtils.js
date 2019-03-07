1b2986c854027f1fcf712bff68ccb50b1d754c57
Julián Adams <jadams@espol.edu.ec>
2019-03-07 13:51:33 -0500

    Pending events: Filter and delete event bad format. Fix #779

--- electron_app/src/utils/RegexUtils.js
+++ electron_app/src/utils/RegexUtils.js
@@ -1,9 +1,11 @@
 const emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
 const HTMLTagsRegex = /<[^>]*>?/g;
 const mailtoProtocolRegex = /^mailto:/;
+const mailformedEventRegex = /(\[|,)null(\]|,)/g;
 
 module.exports = {
   emailRegex,
   HTMLTagsRegex,
-  mailtoProtocolRegex
+  mailtoProtocolRegex,
+  mailformedEventRegex
 };
