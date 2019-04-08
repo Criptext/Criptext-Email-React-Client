0beee8f971d1df8002a3909ab34aec835a18557f
Julián Adams <jadams@espol.edu.ec>
2019-04-08 17:59:31 -0500

    Update Link/Sync devices: Add accountId column

--- electron_app/src/dbExporter.js
+++ electron_app/src/dbExporter.js
@@ -380,6 +380,7 @@ const importDatabaseFromFile = async ({
   let emailContacts = [];
   let emailLabels = [];
   let files = [];
+  let accountContactRelations = [];
   const dbConn = await createDatabaseConnection(databasePath);
 
   return dbConn.transaction(trx => {
@@ -427,10 +428,12 @@ const importDatabaseFromFile = async ({
           switch (table) {
             case Table.CONTACT: {
               contacts.push(object);
+              accountContactRelations.push({ contactId: object.id, accountId });
               if (contacts.length === CONTACTS_BATCH) {
                 lineReader.pause();
                 await insertContactBatch(contacts);
                 contacts = [];
+                accountContactRelations = [];
                 lineReader.resume();
               }
               break;
