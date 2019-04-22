24476557a86e556230ed16bc8c3c7fcbb064733f
erikaperugachi <erikaperugachi@gmail.com>
2019-04-22 17:27:03 -0500

    Update settings view. Close #542

--- email_mailbox/src/components/SettingsGeneralReplyTo.js
+++ email_mailbox/src/components/SettingsGeneralReplyTo.js
@@ -2,49 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch } from 'react-switch-input';
 import string from '../lang';
+import './settingsgeneralreplyto.scss';
 
 const SettingsGeneralReplyTo = props => (
-  <div id="settings-general-reply-to" className="section-block">
-    <div className="section-block-title">
-      <h1>{string.settings.reply_to_email}</h1>
-    </div>
-    <div className="section-block-content">
-      <div className="section-block-content-item content-recovery-email">
-        <div className="settings-switch">
-          <Switch
-            theme="two"
-            name="replyToSwitch"
-            onChange={
-              props.replyToEmail
-                ? props.onRemoveReplyTo
-                : props.onClickSetReplyTo
-            }
-            checked={!!props.replyToEmail}
-            disabled={props.replyToIsLoading}
-          />
-        </div>
-        <div className="settings-switch-label reply-to-email">
-          {props.replyToIsLoading ? (
-            <Loader />
-          ) : (
-            <div>
-              <span className="address">
-                {props.replyToEmail || string.settings.reply_to_not_set}
-              </span>
-              {props.replyToEmail && (
-                <div>
-                  <button
-                    className="button-b"
-                    onClick={() => props.onClickSetReplyTo()}
-                  >
-                    <span>{string.settings.change}</span>
-                  </button>
-                </div>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
+  <div id="settings-general-reply-to" className="cptx-section-item">
+    <span className="cptx-section-item-title">
+      {string.settings.reply_to_email}
+    </span>
+    <span className="cptx-section-item-description">
+      {props.replyToEmail || string.settings.reply_to_not_set}
+    </span>
+    <div className="cptx-section-item-control">
+      {props.replyToEmail &&
+        !props.replyToIsLoading && (
+          <button
+            className="button-b"
+            onClick={() => props.onClickSetReplyTo()}
+          >
+            <span>{string.settings.change}</span>
+          </button>
+        )}
+      {props.replyToIsLoading ? (
+        <Loader />
+      ) : (
+        <Switch
+          theme="two"
+          name="replyToSwitch"
+          onChange={
+            props.replyToEmail ? props.onRemoveReplyTo : props.onClickSetReplyTo
+          }
+          checked={!!props.replyToEmail}
+          disabled={props.replyToIsLoading}
+        />
+      )}
     </div>
   </div>
 );
