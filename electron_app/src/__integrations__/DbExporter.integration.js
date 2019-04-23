a9e38cbac7518b3695432b4e7dd8fded47aecb06
Julián Adams <jadams@espol.edu.ec>
2019-04-22 20:17:13 -0500

    Test: Update DbExporter integration

--- electron_app/src/__integrations__/DbExporter.integration.js
+++ electron_app/src/__integrations__/DbExporter.integration.js
@@ -20,30 +20,35 @@ const {
   importDatabaseFromFile
 } = require('./../dbExporter');
 const fs = require('fs');
-const myAccount = require('../Account');
+const { accounts } = require('./data/accounts.json');
 const { APP_DOMAIN } = require('../utils/const');
 
 const DATABASE_PATH = `${__dirname}/test.db`;
 const PARSED_SAMPLE_FILEPATH = `${__dirname}/parsed_sample_file.txt`;
 const TEMP_DIRECTORY = '/tmp/criptext-tests';
 
-const username = `${myAccount.recipientId}@${APP_DOMAIN}`;
+const accountA = accounts[0];
+const username = `${accountA.recipientId}@${APP_DOMAIN}`;
+const accountId = accountA.id;
 
 const contacts = [
   {
     name: 'Alice',
     email: 'alice@criptext.com',
-    isTrusted: false
+    isTrusted: false,
+    accountId
   },
   {
     name: 'Bob',
     email: 'bob@criptext.com',
-    isTrusted: false
+    isTrusted: false,
+    accountId
   },
   {
     name: 'Charlie',
     email: 'charlie@criptext.com',
-    isTrusted: false
+    isTrusted: false,
+    accountId
   }
 ];
 
@@ -51,12 +56,14 @@ const labels = [
   {
     text: 'Sent',
     color: '000000',
-    uuid: '00000000-0000-0000-0000-000000000001'
+    uuid: '00000000-0000-0000-0000-000000000001',
+    accountId
   },
   {
     text: 'Starred',
     color: '111111',
-    uuid: '00000000-0000-0000-0000-000000000002'
+    uuid: '00000000-0000-0000-0000-000000000002',
+    accountId
   }
 ];
 
@@ -77,7 +84,8 @@ const email = {
     trashDate: null,
     messageId: 'messageId1',
     fromAddress: 'Alice <alice@criptext.com>',
-    replyTo: ''
+    replyTo: '',
+    accountId
   },
   recipients: {
     from: ['Alice <alice@criptext.com>'],
@@ -104,25 +112,31 @@ const file = {
 
 let dbConnection;
 
-const insertContacts = async params => {
-  await DBManager.createContact(params);
+const createAccount = async () => {
+  await DBManager.createAccount(accountA);
 };
 
-const insertLabels = async params => {
-  await DBManager.createLabel(params);
+const insertContacts = async () => {
+  await DBManager.createContact(contacts[0]);
+  await DBManager.createContact(contacts[1]);
+  await DBManager.createContact(contacts[2]);
 };
 
-const insertEmail = async params => {
+const insertLabels = async () => {
+  await DBManager.createLabel(labels);
+};
+
+const insertEmail = async () => {
   fileUtils.saveEmailBody({
-    metadataKey: params.key,
+    metadataKey: email.email.key,
     username,
-    body: params.content
+    body: email.email.content
   });
-  await DBManager.createEmail(params);
+  await DBManager.createEmail(email);
 };
 
-const insertFile = async params => {
-  await DBManager.createFile(params);
+const insertFile = async () => {
+  await DBManager.createFile(file);
 };
 
 const createTempDirectory = () => {
@@ -150,82 +164,90 @@ const cleanTempDirectory = () => {
 beforeAll(async () => {
   await DBManager.cleanDataBase();
   await DBManager.createTables();
+  await createAccount();
 });
 
 beforeEach(async () => {
   await fileUtils.removeUserDir(username);
   await DBManager.cleanDataBase();
   await DBManager.createTables();
+  await createAccount();
 });
 
-afterAll(() => {
-  fileUtils.removeUserDir(username);
+afterAll(async () => {
+  await fileUtils.removeUserDir(username);
   cleanTempDirectory();
   closeDatabaseConnection(dbConnection);
 });
 
 const insertValuesToDatabase = async () => {
-  await insertContacts(contacts);
-  await insertLabels(labels);
-  await insertEmail(email);
-  await insertFile(file);
+  await insertContacts();
+  await insertLabels();
+  await insertEmail();
+  await insertFile();
 };
 
 describe('Parse database: ', () => {
   dbConnection = createDatabaseConnection(DATABASE_PATH);
 
   it('Should parse Contacts to string', async () => {
-    await insertContacts(contacts);
+    await insertContacts();
     const expectedString =
       `{"table":"contact","object":{"id":1,"email":"alice@criptext.com","name":"Alice","isTrusted":false}}\n` +
       `{"table":"contact","object":{"id":2,"email":"bob@criptext.com","name":"Bob","isTrusted":false}}\n` +
       `{"table":"contact","object":{"id":3,"email":"charlie@criptext.com","name":"Charlie","isTrusted":false}}`;
-    const contactsString = await exportContactTable(dbConnection);
+    const contactsString = await exportContactTable(dbConnection, accountId);
     expect(contactsString).toBe(expectedString);
   });
 
   it('Should parse Labels to string', async () => {
-    await insertLabels(labels);
+    await insertLabels();
     const expectedString =
       `{"table":"label","object":{"id":1,"text":"Sent","color":"000000","type":"custom","visible":true,"uuid":"00000000-0000-0000-0000-000000000001"}}\n` +
       `{"table":"label","object":{"id":2,"text":"Starred","color":"111111","type":"custom","visible":true,"uuid":"00000000-0000-0000-0000-000000000002"}}`;
-    const labelsString = await exportLabelTable(dbConnection);
+    const labelsString = await exportLabelTable(dbConnection, accountId);
     expect(labelsString).toBe(expectedString);
   });
 
   it('Should parse Emails to string', async () => {
-    await insertEmail(email);
+    await insertEmail();
     const expectedString = `{"table":"email","object":{"id":1,"key":1,"threadId":"threadA","s3Key":"s3KeyA","subject":"Greetings","content":"<p>Hello there</p>","preview":"Hello there","date":"2013-10-07 08:23:19","status":2,"unread":false,"secure":true,"isMuted":false,"messageId":"messageId1","fromAddress":"Alice <alice@criptext.com>","replyTo":"","unsentDate":"2018-06-14 08:23:20"}}`;
     const expectedJSON = JSON.parse(expectedString);
-    const emailsString = await exportEmailTable(dbConnection);
+    const emailsString = await exportEmailTable(dbConnection, accountId);
     const emailsJSON = JSON.parse(emailsString);
     expect(emailsJSON).toMatchObject(expect.objectContaining(expectedJSON));
   });
 
   it('Should parse relation EmailContact to string', async () => {
-    await insertEmail(email);
+    await insertEmail();
     const expectedString =
       `{"table":"email_contact","object":{"id":1,"contactId":1,"emailId":1,"type":"from"}}\n` +
       `{"table":"email_contact","object":{"id":2,"contactId":2,"emailId":1,"type":"to"}}\n` +
       `{"table":"email_contact","object":{"id":3,"contactId":3,"emailId":1,"type":"to"}}`;
-    const emailContactsString = await exportEmailContactTable(dbConnection);
+    const emailContactsString = await exportEmailContactTable(
+      dbConnection,
+      accountId
+    );
     expect(emailContactsString).toBe(expectedString);
   });
 
   it('Should parse relation EmailLabel to string', async () => {
-    await insertEmail(email);
+    await insertEmail();
     const expectedString =
       `{"table":"email_label","object":{"id":1,"labelId":1,"emailId":1}}\n` +
       `{"table":"email_label","object":{"id":2,"labelId":2,"emailId":1}}`;
-    const emaillabelsString = await exportEmailLabelTable(dbConnection);
+    const emaillabelsString = await exportEmailLabelTable(
+      dbConnection,
+      accountId
+    );
     expect(emaillabelsString).toBe(expectedString);
   });
 
   it('Should parse Files to string', async () => {
-    await insertEmail(email);
-    await insertFile(file);
+    await insertEmail();
+    await insertFile();
     const expectedString = `{"table":"file","object":{"id":1,"token":"token1","name":"Criptext_Image_2018_09_03.png","readOnly":false,"size":183241,"status":1,"date":"2018-09-03 18:45:57","mimeType":"image/png","ephemeral":0,"ephemeralStart":0,"ephemeralTime":0,"emailId":1,"key":"fileKeyA","iv":"fileIvA"}}`;
-    const filesString = await exportFileTable(dbConnection);
+    const filesString = await exportFileTable(dbConnection, accountId);
     expect(filesString).toBe(expectedString);
   });
 });
@@ -241,7 +263,8 @@ describe('Encrypt and Decrypt: ', () => {
     await insertValuesToDatabase();
     await exportDatabaseToFile({
       databasePath: DATABASE_PATH,
-      outputPath
+      outputPath,
+      accountId
     });
     const sampleFile = fs.readFileSync(PARSED_SAMPLE_FILEPATH);
     const resultFile = fs.readFileSync(outputPath);
@@ -272,7 +295,8 @@ describe('Import Database: ', () => {
   it('Should save file data in database', async () => {
     await importDatabaseFromFile({
       filepath: PARSED_SAMPLE_FILEPATH,
-      databasePath: DATABASE_PATH
+      databasePath: DATABASE_PATH,
+      accountId
     });
     const [rawEmail] = await DBManager.getEmailsByThreadId(
       email.email.threadId
@@ -314,12 +338,17 @@ describe('Import Database: ', () => {
       ...fileImported,
       readOnly: !!fileImported.readOnly
     };
-    expect(emailResult).toMatchObject(expect.objectContaining(email.email));
+    const updatedContacts = contacts.map(item => {
+      const contact = Object.assign({}, item);
+      delete contact.accountId;
+      return contact;
+    });
+    expect(emailResult).toMatchObject(email.email);
     expect(fileResult).toMatchObject(file);
     expect(firstLabel).toMatchObject(labels[0]);
     expect(secondLabel).toMatchObject(labels[1]);
-    expect(firstContact).toMatchObject(contacts[0]);
-    expect(secondContact).toMatchObject(contacts[1]);
-    expect(thirdContact).toMatchObject(contacts[2]);
+    expect(firstContact).toMatchObject(updatedContacts[0]);
+    expect(secondContact).toMatchObject(updatedContacts[1]);
+    expect(thirdContact).toMatchObject(updatedContacts[2]);
   });
 });
