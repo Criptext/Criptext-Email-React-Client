c68e56436a4cd4354759c9b305ab12e3bdc66115
Julián Adams <jadams@espol.edu.ec>
2019-05-02 13:44:58 -0500

    Mailbox: Fix incoming email labels. Fix #888

--- email_mailbox/src/utils/electronEventInterface.js
+++ email_mailbox/src/utils/electronEventInterface.js
@@ -353,7 +353,9 @@ const handleNewMessageEvent = async ({ rowid, params }) => {
     : undefined;
   const InboxLabelId = LabelType.inbox.id;
   const SentLabelId = LabelType.sent.id;
-  const isFromMe = recipientId === myAccount.recipientId;
+  const SpamLabelId = LabelType.spam.id;
+  const isFromMe =
+    myAccount.recipientId === getRecipientIdFromEmailAddressTag(from);
   const recipients = getRecipientsFromData({
     to: to || toArray,
     cc: cc || ccArray,
@@ -362,7 +364,7 @@ const handleNewMessageEvent = async ({ rowid, params }) => {
   });
   const isToMe = checkEmailIsTo(recipients);
   let notificationPreview = '';
-  let labelIds = [];
+  const labelIds = [];
   if (!prevEmail) {
     let body = '',
       headers;
@@ -437,12 +439,16 @@ const handleNewMessageEvent = async ({ rowid, params }) => {
           })
         : null;
 
-    labelIds = isSpam ? [LabelType.spam.id] : [];
     if (isFromMe) {
       labelIds.push(SentLabelId);
-    }
-    if (isToMe || (!isFromMe && !isToMe)) {
+      if (isToMe) {
+        labelIds.push(InboxLabelId);
+      }
+    } else {
       labelIds.push(InboxLabelId);
+    }
+    if (isSpam) {
+      labelIds.push(SpamLabelId);
     }
     const emailData = {
       accountId: myAccount.id,
@@ -458,15 +464,20 @@ const handleNewMessageEvent = async ({ rowid, params }) => {
     const prevEmailLabels = await getEmailLabelsByEmailId(prevEmail.id);
     const prevLabels = prevEmailLabels.map(item => item.labelId);
 
-    const hasInboxLabelId = prevLabels.includes(InboxLabelId);
-    if (isToMe && !hasInboxLabelId) {
-      labelIds.push(InboxLabelId);
-    }
-
     const hasSentLabelId = prevLabels.includes(SentLabelId);
     if (isFromMe && !hasSentLabelId) {
       labelIds.push(SentLabelId);
+
+      const hasInboxLabelId = prevLabels.includes(InboxLabelId);
+      if (isToMe && !hasInboxLabelId) {
+        labelIds.push(InboxLabelId);
+      }
     }
+    const hasSpamLabelId = prevLabels.includes(SpamLabelId);
+    if (isSpam && !hasSpamLabelId) {
+      labelIds.push(SpamLabelId);
+    }
+
     if (labelIds.length) {
       const emailLabel = formEmailLabel({
         emailId: prevEmail.id,
@@ -485,10 +496,9 @@ const handleNewMessageEvent = async ({ rowid, params }) => {
       threadId
     });
   }
-  const labelIdsFiltered = isSpam ? [LabelType.spam.id] : labelIds;
   return {
     rowid,
-    labelIds: labelIdsFiltered,
+    labelIds,
     threadIds: threadId ? [threadId] : null
   };
 };
