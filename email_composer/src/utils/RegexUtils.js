f6fd4b24546dcc5ecf70c35f6d08c8f522f90fca
erikaperugachi <erikaperugachi@gmail.com>
2019-05-08 15:24:19 -0500

    Adapt email address regex with replyTo format. Fix #914

--- email_composer/src/utils/RegexUtils.js
+++ email_composer/src/utils/RegexUtils.js
@@ -1,4 +1,4 @@
-export const emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+export const emailRegex = /^(([a-z]*\+)?[\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
 export const HTMLTagsRegex = /<[^>]*>?/g;
 export const contactsRegex = string =>
   new RegExp(`(^${string}| ${string})`, 'i');
