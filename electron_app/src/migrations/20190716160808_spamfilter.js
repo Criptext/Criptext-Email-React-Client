213d6c14c46b7ba70f4c742166da425d92b03d36
erikaperugachi <erikaperugachi@gmail.com>
2019-07-16 16:58:08 -0500

    Create spamScore colum to contact table

---
+++ electron_app/src/migrations/20190716160808_spamfilter.js
@@ -0,0 +1,35 @@
+const { Table } = require('./../models');
+
+const SPAM_SCORE_COLUMN = 'spamScore';
+
+const createSpamScoreColumn = async knex => {
+  const columnExists = await knex.schema.hasColumn(
+    Table.CONTACT,
+    SPAM_SCORE_COLUMN
+  );
+  if (!columnExists)
+    await knex.schema.table(Table.CONTACT, table => {
+      table.integer(SPAM_SCORE_COLUMN).default(0);
+    });
+};
+
+const deleteSpamScoreColumn = async knex => {
+  const columnExists = await knex.schema.hasColumn(
+    Table.CONTACT,
+    SPAM_SCORE_COLUMN
+  );
+  if (columnExists)
+    await knex.schema.table(Table.CONTACT, table => {
+      table.dropColumn(SPAM_SCORE_COLUMN);
+    });
+};
+
+exports.up = async function(knex, Promise) {
+  const addColumn = createSpamScoreColumn(knex);
+  return await Promise.all([addColumn]);
+};
+
+exports.down = async function(knex, Promise) {
+  const dropColumn = deleteSpamScoreColumn(knex);
+  return await Promise.all([dropColumn]);
+};
