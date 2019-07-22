8ef7c96ea2d8846bf750c6ad061d824cdd5f45e9
Julián Adams <jadams@espol.edu.ec>
2019-07-21 23:24:51 -0500

    Refactor BackupManager

--- electron_app/src/BackupManager.js
+++ electron_app/src/BackupManager.js
@@ -1,59 +1,168 @@
+const fs = require('fs');
 const path = require('path');
 const { app } = require('electron');
+const copy = require('recursive-copy');
 const myAccount = require('./Account');
 const { databasePath } = require('./models');
 const { APP_DOMAIN } = require('./utils/const');
-const { createPathRecursive } = require('./utils/FileUtils');
+const { createPathRecursive, getUserEmailsPath } = require('./utils/FileUtils');
 const {
   encryptStreamFile,
   exportDatabaseToFile,
   generateKeyAndIvFromPassword
 } = require('./dbExporter');
 
-const getDefaultBackupFolder = () => {
-  const username = myAccount.recipientId.includes('@')
+/*  Folders & Paths
+----------------------------- */
+const TempBackupFolderName = 'BackupData';
+const TempDatabaseBackupFileName = 'CriptextBackup.db';
+const TempEmailsBackupFolderName = 'EmailsBackup';
+const TempBackupDirectory = path.join(databasePath, '..', TempBackupFolderName);
+const TempDatabaseBackupPath = path.join(
+  TempBackupDirectory,
+  TempDatabaseBackupFileName
+);
+const TempEmailsBackupPath = path.join(
+  TempBackupDirectory,
+  TempEmailsBackupFolderName
+);
+const U_ExportedFileName = path.join(TempBackupDirectory, 'backup.db');
+const E_ExportedFileName = path.join(TempBackupDirectory, 'backup.enc');
+
+/*  Temp Directory
+----------------------------- */
+const checkTempBackupDirectory = () => {
+  try {
+    fs.statSync(TempBackupDirectory);
+  } catch (e) {
+    fs.mkdirSync(TempBackupDirectory);
+  }
+};
+
+const removeTempBackupDirectoryRecursive = pathToDelete => {
+  if (fs.existsSync(pathToDelete)) {
+    fs.readdirSync(pathToDelete).forEach(file => {
+      const currentPath = path.join(pathToDelete, file);
+      if (fs.lstatSync(currentPath).isDirectory()) {
+        removeTempBackupDirectoryRecursive(currentPath);
+      } else {
+        fs.unlinkSync(currentPath);
+      }
+    });
+    fs.rmdirSync(pathToDelete);
+  }
+};
+
+/*  Default Directory
+----------------------------- */
+function getUsername() {
+  return myAccount.recipientId.includes('@')
     ? myAccount.recipientId
     : `${myAccount.recipientId}@${APP_DOMAIN}`;
-  return path.join(app.getPath('documents'), 'Criptext', username, 'backups');
+}
+
+const getDefaultBackupFolder = () => {
+  return path.join(
+    app.getPath('documents'),
+    'Criptext',
+    getUsername(),
+    'backups'
+  );
 };
 
 const createDefaultBackupFolder = () => {
   const backupPath = getDefaultBackupFolder();
-  createPathRecursive(backupPath);
-  return backupPath;
+  return createPathRecursive(backupPath);
 };
 
-const exportBackupFile = async ({ customPath, password }) => {
-  const defaultFilename = password ? 'exported.enc' : 'exported.db';
-  const backupPath =
-    customPath || path.join(getDefaultBackupFolder(), defaultFilename);
-  try {
-    await exportDatabaseToFile({
-      databasePath,
-      outputPath: backupPath
+/*  Methods
+----------------------------- */
+const createTempDatabaseBackup = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile(databasePath, TempDatabaseBackupPath, cpErr => {
+      if (cpErr) reject({ error: 'Preparing backup error' });
+      resolve();
     });
-  } catch (exportErr) {
-    return Promise.reject({ name: 'Export error', error: exportErr });
-  }
-  if (!password) return Promise.resolve(); // Saved in text file
+  });
+};
 
-  const { key, iv } = generateKeyAndIvFromPassword(password);
-  const tmpOutputPath = path.join(backupPath, '..', 'temp.enc');
+const createTempEmailsBackup = () => {
+  const EmailsFolder = getUserEmailsPath(process.env.NODE_ENV, getUsername());
+  return new Promise((resolve, reject) => {
+    try {
+      copy(EmailsFolder, TempEmailsBackupPath);
+      resolve();
+    } catch (error) {
+      reject({ error: 'Preparing backup error' });
+    }
+  });
+};
+
+const prepareBackupFiles = async () => {
   try {
-    await encryptStreamFile({
-      inputFile: backupPath,
-      outputFile: tmpOutputPath,
-      key,
-      iv
-    });
-  } catch (encryptErr) {
-    return Promise.reject({ name: 'Encrypt error', error: encryptErr });
+    checkTempBackupDirectory();
+    await createTempDatabaseBackup();
+    await createTempEmailsBackup();
+  } catch (error) {
+    removeTempBackupDirectoryRecursive(TempBackupDirectory);
+    return error;
   }
-  return Promise.resolve();
+};
+
+const exportBackupFile = async ({ customPath, moveToDest = true }) => {
+  try {
+    try {
+      await exportDatabaseToFile({
+        databasePath: TempDatabaseBackupPath,
+        outputPath: U_ExportedFileName
+      });
+    } catch (dbErr) {
+      throw new Error('Backup database error');
+    }
+    try {
+      if (moveToDest) {
+        fs.writeFileSync(customPath, fs.readFileSync(U_ExportedFileName));
+      }
+    } catch (fileErr) {
+      throw new Error('Backup create file error');
+    }
+  } catch (error) {
+    return error;
+  } finally {
+    removeTempBackupDirectoryRecursive(TempBackupDirectory);
+  }
+};
+
+const encryptBackupFile = async ({ customPath, password }) => {
+  try {
+    try {
+      const { key, iv } = generateKeyAndIvFromPassword(password);
+      await encryptStreamFile({
+        inputFile: U_ExportedFileName,
+        outputFile: E_ExportedFileName,
+        key,
+        iv
+      });
+    } catch (encryptErr) {
+      throw new Error('Encrypting error');
+    }
+    // Move to destination
+    try {
+      fs.writeFileSync(customPath, fs.readFileSync(E_ExportedFileName));
+    } catch (fileErr) {
+      throw new Error('Backup create file error');
+    }
+  } catch (error) {
+    return error;
+  } finally {
+    removeTempBackupDirectoryRecursive(TempBackupDirectory);
+  }
 };
 
 module.exports = {
   createDefaultBackupFolder,
+  getDefaultBackupFolder,
+  prepareBackupFiles,
   exportBackupFile,
-  getDefaultBackupFolder
+  encryptBackupFile
 };
