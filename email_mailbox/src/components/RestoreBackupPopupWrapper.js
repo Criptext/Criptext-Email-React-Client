14e78efbf53c3519b8659c1ca74b3140d502a097
Julián Adams <jadams@espol.edu.ec>
2019-07-22 16:52:17 -0500

    Backup: Import popup wrapper

--- email_mailbox/src/components/RestoreBackupPopupWrapper.js
+++ email_mailbox/src/components/RestoreBackupPopupWrapper.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import RestoreBackupRequest from './RestoreBackupRequest';
-import {
-  setCanceledSyncStatus,
-  showOpenBackupFileDialog
-} from './../utils/electronInterface';
+import RestoreBackupFromFilePopup from './RestoreBackupFromFilePopup';
+import RestoreBackupInvalidFilePopup from './RestoreBackupInvalidFilePopup';
+import RestoreBackupProgressPopupWrapper from './RestoreBackupProgressPopupWrapper';
+import RestoreBackupRequestPopup from './RestoreBackupRequestPopup';
+import { setCanceledSyncStatus } from './../utils/electronInterface';
+import { convertToHumanSize } from './../utils/StringUtils';
+import { formatLastBackupDate } from './../utils/TimeUtils';
 
 const RestoreBackupModes = {
   REQUEST: 'request',
@@ -24,7 +26,12 @@ class RestoreBackupPopupWrapper extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: RestoreBackupModes.REQUEST
+      backupDate: null,
+      backupFileType: null,
+      backupSize: null,
+      mode: RestoreBackupModes.REQUEST,
+      passphrase: '',
+      inputType: 'password'
     };
   }
 
@@ -32,9 +39,43 @@ class RestoreBackupPopupWrapper extends Component {
     switch (this.state.mode) {
       case RestoreBackupModes.REQUEST: {
         return (
-          <RestoreBackupRequest
-            onConfirmRestoreBackup={this.handleConfirmRestoreBackup}
+          <RestoreBackupRequestPopup
+            onSelectBackupFile={this.handleSelectBackupFile}
             onDismissRestoreBackup={this.handleDismissRestoreBackup}
+          />
+        );
+      }
+      case RestoreBackupModes.INVALID_FILE: {
+        return (
+          <RestoreBackupInvalidFilePopup
+            onSelectBackupFile={this.handleSelectBackupFile}
+            onDismissRestoreBackup={this.handleDismissRestoreBackup}
+          />
+        );
+      }
+      case RestoreBackupModes.RESTORING: {
+        return (
+          <RestoreBackupProgressPopupWrapper
+            onClickCancelRestoreBackup={this.handleClickCancelRestoreBackup}
+            passphrase={this.state.passphrase}
+          />
+        );
+      }
+      case RestoreBackupModes.ENCRYPTED_FILE:
+      case RestoreBackupModes.UNENCRYPTED_FILE: {
+        const backupDate = formatLastBackupDate(this.state.backupDate);
+        const backupSize = convertToHumanSize(this.state.backupSize, true, 0);
+        return (
+          <RestoreBackupFromFilePopup
+            backupDate={backupDate}
+            backupFileType={this.state.backupFileType}
+            backupSize={backupSize}
+            inputType={this.state.inputType}
+            onChangeInputPassphrase={this.handleChangeInputPassphrase}
+            onChangeInputTypePassphrase={this.handleChangeInputTypePassphrase}
+            onDismissRestoreBackup={this.handleDismissRestoreBackup}
+            onRestoreBackupFromFile={this.handleRestoreBackupFromFile}
+            passphrase={this.state.passphrase}
           />
         );
       }
@@ -43,16 +84,57 @@ class RestoreBackupPopupWrapper extends Component {
     }
   }
 
-  handleConfirmRestoreBackup = () => {
-    const allowedExtensions = Object.values(BackupTypes);
-    showOpenBackupFileDialog(allowedExtensions, paths => {
-      if (!paths || !paths.length) return;
-      const backupType = paths[0].split('.').pop();
-      const nextMode =
-        backupType === BackupTypes.ENCRYPTED
-          ? RestoreBackupModes.ENCRYPTED_FILE
-          : RestoreBackupModes.UNENCRYPTED_FILE;
-      this.setState({ mode: nextMode });
+  handleSelectBackupFile = ev => {
+    ev.preventDefault();
+    const [file] = ev.target.files;
+    const fileType = file.name.split('.').pop();
+    let newState = {};
+    switch (fileType) {
+      case BackupTypes.ENCRYPTED:
+        newState = {
+          backupFileType: BackupTypes.ENCRYPTED,
+          mode: RestoreBackupModes.ENCRYPTED_FILE,
+          backupDate: file.lastModified,
+          backupSize: file.size
+        };
+        break;
+      case BackupTypes.UNENCRYPTED:
+        newState = {
+          backupFileType: BackupTypes.UNENCRYPTED,
+          mode: RestoreBackupModes.UNENCRYPTED_FILE,
+          backupDate: file.lastModified,
+          backupSize: file.size
+        };
+        break;
+      default:
+        newState = {
+          backupFileType: null,
+          mode: RestoreBackupModes.INVALID_FILE,
+          backupDate: null,
+          backupSize: null
+        };
+        break;
+    }
+    this.setState(newState);
+  };
+
+  handleChangeInputPassphrase = ev => {
+    const value = ev.target.value;
+    this.setState({
+      passphrase: value
+    });
+  };
+
+  handleChangeInputTypePassphrase = () => {
+    const prevValue = this.state.inputType;
+    this.setState({
+      inputType: prevValue === 'password' ? 'text' : 'password'
+    });
+  };
+
+  handleRestoreBackupFromFile = () => {
+    this.setState({
+      mode: RestoreBackupModes.RESTORING
     });
   };
 
@@ -60,10 +142,14 @@ class RestoreBackupPopupWrapper extends Component {
     setCanceledSyncStatus(false);
     this.props.onCloseMailboxPopup();
   };
+
+  handleClickCancelRestoreBackup = () => {
+    this.handleDismissRestoreBackup();
+  };
 }
 
 RestoreBackupPopupWrapper.propTypes = {
   onCloseMailboxPopup: PropTypes.func
 };
 
-export default RestoreBackupPopupWrapper;
+export { RestoreBackupPopupWrapper as default, BackupTypes };
