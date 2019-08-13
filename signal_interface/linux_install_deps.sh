3e7fc53ed7ae829e1723020205bd8281ef56f7a1
Julián Adams <jadams@espol.edu.ec>
2019-08-13 16:45:54 -0500

    Update signal_interface script for linux

--- signal_interface/linux_install_deps.sh
+++ signal_interface/linux_install_deps.sh
@@ -1,25 +1,36 @@
-#!bin/bash
+#!bin/sh
+tempBuildFolder='deps'
 
+# Update repos list
 sudo apt-get update
-sudo apt-get install libssl-dev -y
-npm install -g node-gyp
 
-mkdir deps 
-cd deps
+# Install deps
+sudo apt-get install libssl1.0-dev -y
+# Optional deps. Required in db_interface folder
+sudo apt-get install gcc cmake git pkg-config -y
 
+# Temp build folder
+mkdir "${tempBuildFolder}" && cd "${tempBuildFolder}"
+
+# Download and build libsignal-protocol-c
 git clone https://github.com/signalapp/libsignal-protocol-c.git
-cd libsignal-protocol-c
-mkdir build && cd build && cmake .. && make -j4 install
+cd ./libsignal-protocol-c
+mkdir build && cd build && cmake .. && sudo make -j4 install
 cd ../..
 
+# Download and build cJSON
 git clone https://github.com/DaveGamble/cJSON.git
-cd cJSON
+cd ./cJSON
 mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=Off .. && sudo make -j4 install
-cd ../../
+cd ../..
 
+# Download and build civetweb
 git clone https://github.com/civetweb/civetweb.git
-cd civetweb
-make slib  && sudo cp include/* /usr/include/ && sudo cp libcivetweb.so* /usr/lib/
+cd ./civetweb
+make slib
+sudo cp include/* /usr/include/
+sudo cp libcivetweb.so* /usr/lib/
+cd ../..
 
-cd ../../..
-rm -rf deps
+# Remove temp folder
+rm -rf "${tempBuildFolder}"
