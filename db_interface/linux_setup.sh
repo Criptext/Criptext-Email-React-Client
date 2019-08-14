d2349f3a7fcb2e4ca41908b6151b434c480e0525
Julián Adams <jadams@espol.edu.ec>
2019-08-14 17:40:24 -0500

    Fancy scripts & clean

--- db_interface/linux_setup.sh
+++ db_interface/linux_setup.sh
@@ -1,27 +1,79 @@
-#!bin/sh
+#!bin/bash
 tempBuildFolder='deps'
 
+function PSM() {
+  tput setaf 2;tput bold; echo "$1"; tput sgr0;
+}
+function PEM() {
+  tput setaf 1;tput setab 7; echo "$1"; tput sgr0;
+}
+function removeTempFolder1() {
+  cd ..; rm -rf "${tempBuildFolder}"; return;
+}
+function removeTempFolder3() {
+  cd ../../..; rm -rf "${tempBuildFolder}"; return;
+}
+
 # Update repos list
-# - 'sudo' is required
-sudo apt-get update
+sudo apt-get update > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to update repos list"
+fi
+printf "\n\n"
+
+echo "-----------------------------------------"
+ PSM "             DB Interface";
+echo "-----------------------------------------"
+
+printf "  - Checking latest repos... \n";
+
 
 # Install deps
-sudo apt-get install gcc cmake git pkg-config -y
+printf "  - Installing build dependencies... \n";
+INSTALL_DEPS_ERROR=$( { sudo apt-get install gcc cmake git pkg-config -y > /dev/null; } 2>&1 )
+
+if [ $? -ne 0 ]; then
+  PEM "      Failed to install deps"
+  echo "Reason: "; PEM $INSTALL_DEPS_ERROR;
+  return;
+fi
 
 # Temp folder
-mkdir "${tempBuildFolder}" && cd "${tempBuildFolder}"
+printf "  - Preparing build... \n";
+mkdir "${tempBuildFolder}" > /dev/null;
+cd "${tempBuildFolder}" > /dev/null;
 
 # Download && build SQLite
-git clone https://github.com/SRombauts/SQLiteCpp
-cd ./SQLiteCpp
-mkdir build && cd build
-cmake -DSQLITECPP_BUILD_EXAMPLES=OFF -DSQLITECPP_BUILD_TESTS=OFF ..
-cmake --build . # Ignore Warnings
-sudo make install
-ctest --output-on-failure
+printf "  - Downloading SQLiteCpp... \n";
+git clone https://github.com/SRombauts/SQLiteCpp --quiet;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to download SQLiteCpp";
+  removeTempFolder1;
+fi
 
-# Exit to tempBuildFolder's parent
-cd ../../..
+printf "  - Creating build directory... \n";
+cd ./SQLiteCpp > /dev/null;
+mkdir build && cd build > /dev/null;
 
-# Remove temp folder
-rm -rf "${tempBuildFolder}"
+cmake -DSQLITECPP_BUILD_EXAMPLES=OFF -DSQLITECPP_BUILD_TESTS=OFF .. > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to make SQLiteCpp";
+  removeTempFolder3;
+fi
+
+# Ignore Warnings
+cmake --build . > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to build SQLiteCpp";
+  removeTempFolder3;
+fi
+
+sudo make install > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to install SQLiteCpp";
+  removeTempFolder3;
+fi
+
+# Exit to tempBuildFolder's parent and remove
+removeTempFolder3;
+PSM "    Done.";
--- db_interface/macos_setup.sh
+++ db_interface/macos_setup.sh
@@ -1,20 +1,66 @@
+#!bin/bash
 if ! type "brew" > /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew install cmake
+ > /dev/null;
 
-mkdir deps 
-cd deps
+tempBuildFolder='deps'
 
-git clone https://github.com/SRombauts/SQLiteCpp
-cd SQLiteCpp
-mkdir build 
-cd build
-cmake -DSQLITECPP_INTERNAL_SQLITE=ON -DSQLITECPP_BUILD_EXAMPLES=OFF -DSQLITECPP_BUILD_TESTS=OFF ..
-cmake --build . && make install
-ctest --output-on-failure
-cd ..
+function PSM() {
+  tput setaf 2;tput bold; echo "$1"; tput sgr0;
+}
+function PEM() {
+  tput setaf 1;tput setab 7; echo "$1"; tput sgr0;
+}
+function removeTempFolder1() {
+  cd ..; rm -rf "${tempBuildFolder}"; return;
+}
+function removeTempFolder3() {
+  cd ../../..; rm -rf "${tempBuildFolder}"; return;
+}
 
-cd ..
-rm -rf deps
+echo "-----------------------------------------"
+ PSM "             DB Interface";
+echo "-----------------------------------------"
+
+# Temp folder
+printf "  - Preparing build... \n";
+mkdir "${tempBuildFolder}" > /dev/null;
+cd "${tempBuildFolder}" > /dev/null;
+
+# Download && build SQLite
+printf "  - Downloading SQLiteCpp... \n";
+git clone https://github.com/SRombauts/SQLiteCpp --quiet;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to download SQLiteCpp";
+  removeTempFolder1;
+fi
+
+printf "  - Creating build directory... \n";
+cd ./SQLiteCpp > /dev/null;
+mkdir build && cd build > /dev/null;
+
+cmake -DSQLITECPP_INTERNAL_SQLITE=ON -DSQLITECPP_BUILD_EXAMPLES=OFF -DSQLITECPP_BUILD_TESTS=OFF .. > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to make SQLiteCpp";
+  removeTempFolder3;
+fi
+
+# Ignore Warnings
+cmake --build . > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to build SQLiteCpp";
+  removeTempFolder3;
+fi
+
+make install > /dev/null;
+if [ $? -ne 0 ]; then
+  PEM "    Failed to install SQLiteCpp";
+  removeTempFolder3;
+fi
+
+# Exit to tempBuildFolder's parent and remove
+removeTempFolder3;
+PSM "    Done.";
