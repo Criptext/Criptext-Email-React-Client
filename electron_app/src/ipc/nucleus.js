e727972c747fd05075590f6513d2c773a8963d32
Pedro Aim <pero_aim@hotmail.com>
2020-06-02 14:32:35 -0500

    report all app crashes

--- electron_app/src/ipc/nucleus.js
+++ electron_app/src/ipc/nucleus.js
@@ -4,3 +4,7 @@ const { addEventError, NUCLEUS_EVENTS } = require('./../nucleusManager');
 ipc.answerRenderer('nucleups-report-content-unencrypted', error =>
   addEventError(NUCLEUS_EVENTS.REPORT_CONTENT_UNENCRYPTED, error)
 );
+
+ipc.answerRenderer('nucleups-report-uncaught-error', error =>
+  addEventError(NUCLEUS_EVENTS.UNCAUGHT_ERROR, error)
+);
